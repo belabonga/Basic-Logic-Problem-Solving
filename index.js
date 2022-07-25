379a11af593cd73e3944b4d7e86523ff86caf709
Shania <shania1faza@gmail.com>
2022-07-26 05:26:50 +0700

    salah ketuker masukin index.js

--- index.js
+++ index.js
@@ -1,48 +1,70 @@
-// RELEASE 0 - GENERATE BOARD
-let row = 5; col = 4;
+/* PSEUDOCODE 
+STORE 31 AS tanggal
+STORE [] AS array
+LOOP i IN tanggal
+
+IF tanggal MODULUS 5
+DISPLAY "Tanggal" CONCAT WITH tanggal CONCAT WITH ": Tempat Fitness Tutup"
+ELSE IF tanggal modulus 5
+DISPLAY "Tanggal" CONCAT WITH tanggal CONCAT WITH ": Tono, Anton, Budi"
+ELSE 
+    IF tanggal - 7 THEN MODULUS 2 IS 0 (untuk memulai dari tanggal 7)
+        ADD "Tono" TO array
+    IF tanggal - 7 THEN MODULUS 4 IS 0
+        ADD "Anton" TO array
+    IF tanggal - 7 THEN MODULUS 5 IS 0
+        ADD "Budi" TO ARRAY
+
+DISPLAY "Tanggal" CONCAT WITH tanggal CONCAT WITH " : " JOIN>ARRAY
+*/
 
 
-function generateBoard(row, col) {
-    let arr = [];
-    for (let i = 0; i < row; i++) {
-        let inArr = [];
-        for (let j = 0; j < col; j++) {
-            let letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            let random = letter.charAt(Math.floor(Math.random() * letter.length))
-            inArr.push(random)
+
+let tgl = 31;
+for (let i = 7; i <= tgl; i++) {
+    let result = []
+    if (i % 5 === 0) {
+        console.log(`Tanggal ${i}: Tempat Fitness Tutup`)
+    } else if (i === 7) {
+        console.log(`Tanggal ${i}: Tono, Anton, Budi`)
+    } else {
+        if ((i - 7) % 2 === 0) {
+            result.push("Tono")
         }
-        arr.push(inArr)
-    }
-    return arr
-}
-// console.log(generateBoard(5, 4))
-
-// RELEASE 1 - Get Block Filled with Vowels
-function block(row, col) {
-    let arr = generateBoard(row, col);
-    let result = 0
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr[i].length - 1; j++) {
-            let newArr = []
-            newArr.push(arr[i][j])
-            newArr.push(arr[i][j +1])
-            newArr.push(arr[i+1][j])
-            newArr.push(arr[i+1][j+1])
-
-            let vowels = "AIUEO"
-            let counterVowels = 0;
-            for (l of newArr) {
-                for (k of vowels) {
-                    if (l === k) {
-                        counterVowels += 1
-                    }
-                }
-            }
-            if (counterVowels === 4) {
-                result += 1
-            }
+        if ((i - 7) % 4 === 0) {
+            result.push("Anton")
         }
+        if ((i - 7) % 5 === 0) {
+            result.push("Budi")
+        }
+        console.log(`Tanggal ${i}: ${result.join(", ")}`)
     }
-    return result;
 }
-console.log(generateBoard(5, 4), block(5, 4))
+
+/* OUTPUT
+Tanggal 7: Tono, Anton, Budi
+Tanggal 8: 
+Tanggal 9: Tono
+Tanggal 10: Tempat Fitness Tutup
+Tanggal 11: Tono, Anton
+Tanggal 12: Budi
+Tanggal 13: Tono
+Tanggal 14: 
+Tanggal 15: Tempat Fitness Tutup
+Tanggal 16: 
+Tanggal 17: Tono, Budi
+Tanggal 18: 
+Tanggal 19: Tono, Anton
+Tanggal 20: Tempat Fitness Tutup
+Tanggal 21: Tono
+Tanggal 22: Budi
+Tanggal 23: Tono, Anton
+Tanggal 24: 
+Tanggal 25: Tempat Fitness Tutup
+Tanggal 26: 
+Tanggal 27: Tono, Anton, Budi
+Tanggal 28: 
+Tanggal 29: Tono
+Tanggal 30: Tempat Fitness Tutup
+Tanggal 31: Tono, Anton
+*/
